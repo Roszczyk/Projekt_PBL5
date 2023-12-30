fbd9ce03a2259e065f726477a194a79746a31759
Alicja Lakomska <alagracz@gmail.com>
2023-12-30 10:42:57 +0100

    change of timezone in timestamp

--- Aplikacja/app/src/main/java/com/example/hive/MainActivity.kt
+++ Aplikacja/app/src/main/java/com/example/hive/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.hive
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.RelativeLayout
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,38 +25,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         ValTemp = findViewById(R.id.temperature)
         ValHum = findViewById(R.id.humidity)
         ValOpen = findViewById(R.id.openstatus)
         ValLight= findViewById(R.id.lightstatus)
         ValUpdate= findViewById(R.id.updated_at)
         progressBar = findViewById(R.id.loader)
-
-            // show the progress bar
-            progressBar.visibility = View.VISIBLE
+        progressBar.visibility = View.VISIBLE
 
 
-            ApiCall().getsensor(this) { payload ->
-                var temp =payload.temperature.toString()
-                var hum =payload.humidity.toString()
+        ApiCall().getsensor(this) {payload ->
+            var temp =payload.temperature.toString()
+            var hum =payload.humidity.toString()
 
                 //TO DO when new version of payload NO GPS
-                ValOpen.text = payload.gps_lon.toString()
-                ValLight.text = payload.gps_lat.toString()
+            ValOpen.text = payload.gps_lon.toString()
+            ValLight.text = payload.gps_lat.toString()
                 //to this line
+            //Setting correct timestamp of update for last update
+            val currentTimestamp = System.currentTimeMillis()
+            val timeZone = TimeZone.getTimeZone("Europe/Warsaw")
+            val dateFormat = SimpleDateFormat("dd MMM yyyy, HH:mm", Locale("pl", "PL"))
+            
+            dateFormat.timeZone = timeZone
+            ValUpdate.text = dateFormat.format(Date(currentTimestamp))
+            ValTemp.text = "$temp°C"
+            ValHum.text = "$hum%"
+            progressBar.visibility = View.GONE
 
-                //Setting correct time stamp of update
-                val currentTimestamp = System.currentTimeMillis()
-                val dateFormat = SimpleDateFormat(" dd MMM yyyy, HH:mm", Locale("en", "PL"))
-                ValUpdate.text =  dateFormat.format(Date(currentTimestamp))
+        }
 
-                ValTemp.text = "$temp°C"
-                ValHum.text = "$hum%"
-                progressBar.visibility = View.GONE
-            }
 
-//
     }
 }
