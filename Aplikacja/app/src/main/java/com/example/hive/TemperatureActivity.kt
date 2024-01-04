882c18a0bd48ce6197d8a63ac6e69c927352a3bf
Alicja Lakomska <alagracz@gmail.com>
2024-01-04 21:41:12 +0100

    graph one digit after dot

--- Aplikacja/app/src/main/java/com/example/hive/TemperatureActivity.kt
+++ Aplikacja/app/src/main/java/com/example/hive/TemperatureActivity.kt
@@ -1,39 +1,28 @@
 package com.example.hive
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
-import android.icu.text.SimpleDateFormat
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.AxisBase
-import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IValueFormatter
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import org.json.JSONArray
+import com.github.mikephil.charting.utils.ViewPortHandler
 import java.math.BigDecimal
 import java.math.RoundingMode
-import java.time.format.DateTimeFormatter
-import java.util.Locale
+import java.text.DecimalFormat
 
 class TemperatureActivity : AppCompatActivity() {
 
@@ -46,7 +35,7 @@ class TemperatureActivity : AppCompatActivity() {
         lineChart = findViewById(R.id.charttemp)
         imageBack = findViewById(R.id.btnBack)
 
-        val url = "http://10.0.2.2:5000/data/temp-hum"
+        val url = "http://10.0.2.2:5000/data/temp-hum-chart"
 
 
         val request = JsonObjectRequest(
@@ -54,6 +43,7 @@ class TemperatureActivity : AppCompatActivity() {
             { response->
 
                 var dataArray = response.getJSONArray("data")
+
                 if(dataArray.toString()=="[]"){
                     Toast.makeText(applicationContext,"Sorry, could not download the data",Toast.LENGTH_SHORT).show()
                     val intent2 = Intent(this, MainActivity::class.java)
@@ -63,11 +53,13 @@ class TemperatureActivity : AppCompatActivity() {
                 var evenCount =0
                 val dataList = ArrayList<DataTempHum>()
                 val timeList =ArrayList<String>()
-                for (i in (dataArray.length()-5) until dataArray.length()) {
+                for (i in dataArray.length()-1 downTo 0) {
                 //for (i in 0 until 15) {
                     val resultObject = dataArray.getJSONObject(i)
                     val humidity = resultObject.getDouble("humidity")
-                    val temperature = resultObject.getDouble("temperature")
+                    val temptemp = resultObject.getDouble("temperature")
+                    val temperature =  BigDecimal(temptemp).setScale(2, RoundingMode.HALF_EVEN)
+
                     val timestamp = resultObject.getString("timestamp")
                     val dataTempHum = DataTempHum(humidity, temperature, timestamp)
                     dataList.add(dataTempHum)
@@ -76,6 +68,7 @@ class TemperatureActivity : AppCompatActivity() {
                 }
 
                 setUpLineChart(timeList)
+
                 setDataToLineChart(dataList)
 
 
@@ -105,12 +98,16 @@ class TemperatureActivity : AppCompatActivity() {
 
             axisRight.isEnabled = false
             description.isEnabled = false
+            lineChart.animateX(1000);
 
             xAxis.position = XAxis.XAxisPosition.BOTTOM
             xAxis.granularity = 1F
             xAxis.setDrawGridLines(false)
-            xAxis.setDrawAxisLine(false)
+            xAxis.setDrawLabels(true)
             xAxis.valueFormatter=IndexAxisValueFormatter(timeList)
+            //lineData.setValueFormatter()
+
+
 
 
 
@@ -127,8 +124,26 @@ class TemperatureActivity : AppCompatActivity() {
 
 
 
+
+
         }
     }
+
+    inner class MyDataFormatter : IValueFormatter {
+
+        private var mFormat: DecimalFormat? = DecimalFormat("##.#") // use one decimal
+        override fun getFormattedValue(
+            value: Float,
+            entry: Entry?,
+            dataSetIndex: Int,
+            viewPortHandler: ViewPortHandler?
+        ): String? {
+            // write your logic here
+            return mFormat!!.format(value) // e.g. append a dollar-sign
+        }
+
+    }
+
 
     private fun setDataToLineChart(dataTempHumList: List<DataTempHum>) {
 
@@ -142,6 +157,9 @@ class TemperatureActivity : AppCompatActivity() {
         lineTemp.fillColor=Color.BLUE
 
 
+        lineTemp.valueFormatter = MyDataFormatter()
+
+
         val dataSet = ArrayList<ILineDataSet>()
         dataSet.add(lineTemp)
         val lineData = LineData(dataSet)
@@ -150,32 +168,33 @@ class TemperatureActivity : AppCompatActivity() {
 
 
 
+
+
+
+
     }
 
 
     private fun temp(dataTempHumList: List<DataTempHum>): ArrayList<Entry> {
-        val sales = ArrayList<Entry>()
+        val temp = ArrayList<Entry>()
 
         for ((index, data) in dataTempHumList.withIndex()) {
             // Assuming you want to use the timestamp as x-axis and temperature as y-axis
             //val timestampMillis = java.text.SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z").parse(data.timestamp).time
             val temperature = data.temperature?.toFloat() ?: 0f
-           // val timestamp = data.timestamp?.toFloat()?:0f
 
-           // xAxisValueList.add("03:25:26"
-
-
-           // if (timestampMillis != null) {
-                sales.add(Entry(index.toFloat(), temperature))
+                temp.add(Entry(index.toFloat(), temperature))
             //}
         }
 
-        return sales
+        return temp
     }
 
 
 
 
 }
+
+
 
 
