f0b8b5a4566ffa8e2280376bb204df312bb56864
roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2024-01-19 00:12:54 +0100

    @Brudnopis: GPIO Pin Reading

--- Brudnopis/main.c
+++ Brudnopis/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "periph/gpio.h"
+#include "xtimer.h"
 
 int main(void) {
     gpio_t pin = GPIO_PIN(PORT_A, 0);
      if (gpio_init(pin, GPIO_IN) == 0) {
         printf("GPIO initialized successfully.\n");
-    int value = gpio_read(pin);
-    
-        printf("Value read from the pin: %d\n", value);
+    	for(int i=0;i<100;++i){
+		int value = gpio_read(pin);
+        	printf("Value read from the pin: %d\n", value);
+		xtimer_sleep(1);
+	}
     } else {
         printf("Failed to initialize GPIO.\n");
     }
