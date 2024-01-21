2b0c75127998621124a503493fc9652e1db4f5ec
roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2024-01-21 13:53:02 +0100

    On edge modifications

--- Brudnopis/main.c
+++ Brudnopis/main.c
@@ -2,13 +2,16 @@
 #include "periph/gpio.h"
 #include "xtimer.h"
 #include "shell.h"
-#include "semtech_loramac.h"
+//#include "semtech_loramac.h"
 
 int main(void) {
+	gpio_t pin1;
+	pin1=GPIO_PIN(PORT_A, 3);
+	gpio_init(pin1, GPIO_OUT);
     while(1){
-    gpio_set(PORT_A, 3);
+    gpio_set(pin1);
     xtimer_sleep(3);
-    gpio_clear(PORT_A, 3);
+    gpio_clear(pin1);
     xtimer_sleep(3);
     printf("Lap end\n");
     }
